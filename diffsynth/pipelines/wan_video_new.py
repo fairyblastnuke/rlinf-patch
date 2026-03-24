056594cc207ec2db9070b7316b5ff40e214e5c82
fairyblastnuke <tarasle@icloud.com>
2026-03-24 13:25:09 -0400

    token

--- diffsynth/pipelines/wan_video_new.py
+++ diffsynth/pipelines/wan_video_new.py
@@ -599,9 +599,10 @@ class WanVideoPipeline(BasePipeline):
             pipe.width_division_factor = pipe.vae.upsampling_factor * 2
 
         # Initialize tokenizer
-        # tokenizer_config.download_if_necessary(use_usp=use_usp)
         pipe.prompter.fetch_models(pipe.text_encoder)
-        pipe.prompter.fetch_tokenizer(tokenizer_config.path)
+        if tokenizer_config is not None:
+            tokenizer_config.download_if_necessary(use_usp=use_usp)
+            pipe.prompter.fetch_tokenizer(tokenizer_config.path)
 
         if audio_processor_config is not None:
             audio_processor_config.download_if_necessary(use_usp=use_usp)
